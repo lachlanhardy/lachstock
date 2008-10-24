cd4db229fbc083e9f302f4e975c2483235aa7b30
Lachlan Hardy <lachlan@lachstock.com.au>
2008-10-24 17:03:00 +1100

    adding comments (shock, horror)

--- static/inc/js/commits-atom.js
+++ static/inc/js/commits-atom.js
@@ -41,8 +41,8 @@ function githubCallback(json) {
         }
       });
       
+      // grabbing details from URL
       var v = url.match(/http:\/\/github.com\/([^\/]*)\/([^\/]*)\/commit\/([^\/]*)$/);
-      
       var user = v[1];
       var repo = v[2];
       var id = v[3];
@@ -51,44 +51,52 @@ function githubCallback(json) {
       $.getJSON("http://github.com/api/v1/json/" + repoName + "/commit/" + id + "?callback=?",
         function(data){
           
+          // creating initial elements
           var dl = $(document.createElement("dl"));
           var dt = $(document.createElement("dt"));
-          var strong = $(document.createElement("strong"));
+          var dtStrong = $(document.createElement("strong"));
         
+          // linking username with REL microformat
           var dtUser = $(document.createElement("a"));
-          dtUser.attr("href", "http://github.com/" + username).text(username);
-          strong.append(dtUser).append(titleText);
+          dtUser.attr("href", "http://github.com/" + username).attr("rel", "me").text(username);
           
+          // Adding username and eventText to dtStrong
+          dtStrong.append(dtUser).append(titleText);
+          
+          // Linking repo name
           var repoAnchor = $(document.createElement("a"));
           repoAnchor.attr("href", "http://github.com/" + repoName).text(repoName);
-          strong.append(repoAnchor);
+          dtStrong.append(repoAnchor);
           
+          // Building date for prettifying
           var dateSpan = $(document.createElement("span"));
           var theDate = parseDate(data.commit.committed_date); // converts date to format Pretty Date recognises
           dateSpan.addClass("date").text(theDate).attr("title", theDate);
           dateSpan.prettyDate();
           setInterval(function(){ dateSpan.prettyDate(); }, 5000);
           
-          // add STRONG and dateSpan to DT
-          dt.append(strong).append(dateSpan);
+          // add dtStrong and dateSpan to DT
+          dt.append(dtStrong).append(dateSpan);
           
+          // Linking message to commit
           var ddMessage = $(document.createElement("dd"));
           messageAnchor = $(document.createElement("a"));
           messageAnchor.attr("href", url).text(data.commit.message);
           ddMessage.append(messageAnchor);
           
+          // Listing all changed files
           var ddFilenames = $(document.createElement("dd"));
           ddFilenames.addClass("filenames");
-          
           var filenamesUl = $(document.createElement("ul"));
-
           $(data.commit.modified).each(function(i){
             filenamesUl.append($(document.createElement("li")).text(data.commit.modified[i].filename));
           });
           ddFilenames.append(filenamesUl);
           
+          // Adding content to DL
           dl.append(dt).append(ddMessage).append(ddFilenames);
           
+          // Replacing static HTML with new hottness
           $("#github p").replaceWith(dl);
           
         } 
