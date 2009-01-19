395cd7ca2a22e9fce8b6541247315c0d4255a085
Lachlan Hardy <lachlan@lachstock.com.au>
2009-01-20 07:34:17 +1100

    now grabbing pic at random from JSON

--- inc/js/flickr-pic.js
+++ inc/js/flickr-pic.js
@@ -1,11 +1,15 @@
 
 function flickrPic() {
-    $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?tags=lachlanhardy&tagmode=any&format=json&jsoncallback=?",
+    $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?tags=lachlanhardy&format=json&jsoncallback=?",
         function(data){
-          $.each(data.items, function(i,item){
+            var arr = jQuery.makeArray(data);
+            var rndNum = Math.ceil(Math.random() * (data.items.length - 1));
+            var item = data.items[rndNum];
+            
             var img = $("<img/>").attr("src", item.media.m);
-            $("<a/>").attr("href", item.link).append(img).appendTo("#flickr-pic");
-            if ( i == 0 ) return false;
-          });
+            $("<a/>")
+                .attr("href", item.link)
+                .append(img)
+                .appendTo("#flickr-pic");
         });
 }
