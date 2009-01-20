619df5059b9563611480356b13368fd499694976
Lachlan Hardy <lachlan@lachstock.com.au>
2009-01-20 20:59:28 +1100

    adding basic Raphael functionality

--- inc/js/flickr-pic.js
+++ inc/js/flickr-pic.js
@@ -1,4 +1,3 @@
-
 function flickrPic() {
     $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?tags=lachlanhardy&format=json&jsoncallback=?",
         function(data){
@@ -6,22 +5,42 @@ function flickrPic() {
             var rndNum = Math.ceil(Math.random() * (data.items.length - 1));
             var item = data.items[rndNum];
             
-            var img = $("<img/>").attr("src", item.media.m);
             $("<a/>")
                 .attr("href", item.link)
-                .append(img)
-                .appendTo("#flickr-pic");
+                .attr("id", "polaroid")
+                .prependTo("#flickr-pic");
+            
+            var imgWidth = parseInt(item.description.match(/width="(\d*)/)[1]);
+            var imgHeight = parseInt(item.description.match(/height="(\d*)/)[1]);
+                       
+            var paperWidth = (imgWidth + 40);
+            var paperHeight = (imgHeight + 60);
                 
+            var r = Raphael("polaroid", paperWidth + 100, paperHeight + 100);
+            
+            r.rect(55, 63, paperWidth, paperHeight).attr({
+              fill: "#000",
+              opacity: .15,
+              stroke: "#000",
+              "stroke-width": 4,
+              "stroke-opacity": .3
+            }).rotate(10);
+
+            r.rect(50, 55, paperWidth, paperHeight).attr({
+              fill: "#fff",
+              stroke: "#ddd",
+              "stroke-width": 2,
+              "stroke-opacity": .3
+            }).rotate(11);   
+
+            r.image(item.media.m, 70, 75, imgWidth, imgHeight).rotate(11);
+            
             var author = item.author;
             author = author.match(/\(([a-zA-z0-9 *]*)\)/);
             
-            var attributionLink = $("<a/>")
-                                            .attr("href", item.link)
-                                            .append("Image by " + author[1]);
+            r.text(paperWidth - 70, paperHeight + 40, "Image: " + author[1])
+                .attr({"font": '700 10px "Zapfino", "Marker Felt", "Papyrus", "URW Chancery L"'})
+                .rotate(9);
             
-            $("<p/>")
-                    .addClass("attribution")
-                    .append(attributionLink)
-                    .appendTo("#flickr-pic");
         });
 }
