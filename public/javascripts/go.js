abb0badc93072c79490d54f666c48c0fcccdf78a
Lachlan Hardy <lachlan@lachstock.com.au>
2009-07-13 16:47:30 +1000

    Using Remy Sharp's awesome HTML5 shiv

--- public/javascripts/go.js
+++ public/javascripts/go.js
@@ -1,14 +1,6 @@
 $(document).ready(function(){
   // making sexy unobtrusive CSS possible since 2006
   $("html").addClass("js");
-  
-  // Adding some trickery for HTML 5 in IE
-  document.createElement('header');
-  document.createElement('footer');
-  document.createElement('section');
-  document.createElement('aside');
-  document.createElement('nav');
-  document.createElement('article');
 
   addTwitter();
   drawGraphs();
