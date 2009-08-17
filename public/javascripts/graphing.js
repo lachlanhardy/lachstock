0359b720dbd7735d0aa97487581c853e9c42bc6c
Lachlan Hardy <lachlan@lachstock.com.au>
2009-08-17 23:26:25 +1000

    tidying up graph and adding HREFs to legend labels

--- public/javascripts/graphing.js
+++ public/javascripts/graphing.js
@@ -1,15 +1,17 @@
 var drawGraphs = function () {
   if ($("ul.taglist").length != 0) {
 
+    // grab the data values from the page 
     var tags = [],
-      tag_count = [],
-      tag_label = [],
-      tag_href = [];
+        tag_count = [],
+        tag_label = [],
+        tag_href = [];
     $("a:not(.common)", "ul.taglist").each(function(i) {
       var $tag = $(this);
       tags.push({value: parseFloat($tag.attr("data-tag_count")), text: $tag.text(), href: $tag.attr("href")});
     });
     
+    // structure the data
     tags = tags.sort(function (a, b) {
       return b.value - a.value;
     }).splice(0, 10);
@@ -19,11 +21,20 @@ var drawGraphs = function () {
       tag_href.push(tags[i].href);
     }
     
+    // Create a canvas
     $("<div id=\"graph\"/>").insertAfter($("h1", "#tagspace"));
     var r = Raphael("graph", "30em", "16em");
+    
+    // Draw piechart
     var pie = r.g.piechart(300, 120, 100, tag_count, {legend: tag_label, legendpos: "west", href: tag_href});
     pie.labels.attr({font: '1.1em "Helvetica Neue"', translation: "-50 0"});
 
+    // Assign hrefs to legend labels
+    $(pie.labels).each(function (i) {
+      pie.labels[i].attr({href: tag_href[i]});
+    });
+
+    // Set up funky hover states
     pie.hover(function () {
         this.sector.stop();
         this.sector.animate({scale: [1.1, 1.1, this.cx, this.cy]}, 500, "elastic");
