d2fd04d19bed055f6bb0584dc6adaa919d329804
Lachlan Hardy <lachlan@lachstock.com.au>
2010-01-05 20:30:18 +1100

    Adding Dan Webb's delicious Code Highlighter mojo, plus styling for HTML

---
+++ public/javascripts/codehighlighter-css.js
@@ -0,0 +1,23 @@
+/*requires code_highlighter.js*/
+
+CodeHighlighter.addStyle("css", {
+	comment : {
+		exp  : /\/\*[^*]*\*+([^\/][^*]*\*+)*\//
+	},
+	keywords : {
+		exp  : /@\w[\w\s]*/
+	},
+	selectors : {
+		exp  : "([\\w-:\\[.#][^{};>]*)(?={)"
+	},
+	properties : {
+		exp  : "([\\w-]+)(?=\\s*:)"
+	},
+	units : {
+		exp  : /([0-9])(em|en|px|%|pt)\b/,
+		replacement : "$1<span class=\"$0\">$2</span>"
+	},
+	urls : {
+		exp  : /url\([^\)]*\)/
+	}
+ });
