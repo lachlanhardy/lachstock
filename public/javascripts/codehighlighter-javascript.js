1bd358bdf8c4377fd722c478958ef1ac7a58f606
Lachlan Hardy <lachlan@lachstock.com.au>
2010-01-08 17:47:42 +1100

    Sort out some JS syntax highlighting

--- public/javascripts/codehighlighter-javascript.js
+++ public/javascripts/codehighlighter-javascript.js
@@ -11,7 +11,13 @@ CodeHighlighter.addStyle("javascript",{
 		exp  : /'[^'\\]*(\\.[^'\\]*)*'|"[^"\\]*(\\.[^"\\]*)*"/
 	},
 	keywords : {
-		exp  : /\b(arguments|break|case|continue|default|delete|do|else|false|for|function|if|in|instanceof|new|null|return|switch|this|true|typeof|var|void|while|with)\b/
+		exp  : /\b(arguments|break|case|continue|default|delete|do|else|false|for|function|if|in|instanceof|length|new|null|return|switch|this|true|typeof|value|var|void|while|with)\b/
+	},
+	symbols : {
+	  exp  : /\$|=/
+	},
+	numbers : {
+	  exp  : /\b\d+\b/
 	},
 	global : {
 		exp  : /\b(toString|valueOf|window|element|prototype|constructor|document|escape|unescape|parseInt|parseFloat|setTimeout|clearTimeout|setInterval|clearInterval|NaN|isNaN|Infinity)\b/
