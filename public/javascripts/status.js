7134a9a3ecf1169c2346ab9353c0fa1247fefd48
Lachlan Hardy <lachlan@lachstock.com.au>
2010-02-25 00:11:26 +1100

    refactoring status.js

--- public/javascripts/status.js
+++ public/javascripts/status.js
@@ -2,20 +2,22 @@
 
 function twitterCallback2(json) {
 
-  var status = $(document.createElement("strong"));
-  var twitters = json;
-  var username = "";
-  var tweetText = "";
+  var $status = $("<strong/>"),
+      twitters = json,
+      $statusLink = $("<a>Permalink</a>"),
+      $para = $("<p/>"),
+      username,
+      tweetText,
+      statusText;
   
   for (var i=0, ii = twitters.length; i<ii; i++){
     username = twitters[i].user.screen_name;
-    
     tweetText = twitters[i].text;
         
     if (tweetText.substr(0,1) != "@"){
       
       // URL regex. I think that's everything, but it's probably not
-      var statusText = twitters[i].text.replace(/((ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?)/gi,'<a href="$1">$1</a>');
+      statusText = twitters[i].text.replace(/((ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?)/gi,'<a href="$1">$1</a>');
       
       // real comments
       statusText = statusText.replace(/(\s@+[a-zA-Z_]{1,})/gi,'<a href="http://twitter.com/$1">$1</a>');
@@ -23,10 +25,9 @@ function twitterCallback2(json) {
       // real comments
       statusText = statusText.replace(/(http:\/\/twitter.com\/\s@)/gi, 'http://twitter.com/');
       
-      $(status).html(statusText);  
+      $status.html(statusText);
       
-      var statusLink = $("<a>Permalink</a>"); 
-      statusLink.attr({
+      $statusLink.attr({
         href: "http://twitter.com/" + username + "/statuses/" + twitters[i].id,
         id: "status-link"
       });
@@ -36,20 +37,20 @@ function twitterCallback2(json) {
   }
   
   // just in case I've managed to do 10 replies without a straight-up tweet
-  if (!$(status).text() == "") {
-    var para = $(document.createElement("p"));
-    para.append(status).append(statusLink);
-
-    $("#status").empty().append(para);
+  if (!$status.text() == "") {
+    $para.append($status).append($statusLink);
+    $("#status").empty().append($para);
   }
 }
 
 var addTwitter = function() {
   if ($("#twitter-avatar")[0]) {
-    // drawing bubbles
-    var paper = Raphael("twitter-avatar", "100", "33"),
-        bubbles = paper.set();
 
+    var paper = Raphael("twitter-avatar", "100", "33"),
+        bubbles = paper.set(),
+        $script = $("<script/>").attr("src", "http://twitter.com/statuses/user_timeline/lachlanhardy.json?callback=twitterCallback2&count=10");
+
+    // drawing bubbles
     bubbles.push(
         paper.circle(22, 9, 7),
         paper.circle(31, 18, 5),
@@ -61,11 +62,6 @@ var addTwitter = function() {
       });
 
     // Calling Twitter feed
-    if ($("#status").length != 0) {
-      var script = $(document.createElement("script"));
-      script.attr("src", "http://twitter.com/statuses/user_timeline/lachlanhardy.json?callback=twitterCallback2&count=10");
-
-      $("body").append(script);
-    }
+    $("body").append($script);
   }
 };
