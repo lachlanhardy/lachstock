773e749115aa7e4f53919d4957d43398b38bb001
Lachlan Hardy <lachlan@lachstock.com.au>
2010-08-30 10:11:44 +1000

    gabbertalk

--- public/javascripts/go.js
+++ public/javascripts/go.js
@@ -11,4 +11,10 @@ $(document).ready(function(){
   $("#flickr-pic").flickrPolaroid();
   // githubActivity();
   
+  
+  var addClient = document.createElement('script'); 
+  addClient.src = "http://gabbertalk.com/845c0a1287da5c09377574389f310931/bootstrap.js";
+  $("body").append(addClient);
+
+  
 });
