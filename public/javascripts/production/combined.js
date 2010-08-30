56fae822926531c034ca70167b437c64486c7729
Lachlan Hardy <lachlan@lachstock.com.au>
2010-08-30 11:51:58 +1000

    Minifying changed JS & CSS for production

--- public/javascripts/production/combined.js
+++ public/javascripts/production/combined.js
@@ -10014,12 +10014,7 @@ $(document).ready(function(){
   drawGraphs();
   $("#flickr-pic").flickrPolaroid();
   // githubActivity();
-  
-  
-  var addClient = '<script src="http://gabbertalk.com/845c0a1287da5c09377574389f310931/bootstrap.js?client=false"></script>'
-  $("body").append(addClient);
 
-  
 });
 
 /*! Unobtrustive Code Highlighter By Dan Webb 11/2005
