4b0a99a112f750b05df2753662719ebd178cd94a
Lachlan Hardy <lachlan@lachstock.com.au>
2010-08-30 11:51:41 +1000

    making GabberTalk work

--- public/javascripts/go.js
+++ public/javascripts/go.js
@@ -10,10 +10,5 @@ $(document).ready(function(){
   drawGraphs();
   $("#flickr-pic").flickrPolaroid();
   // githubActivity();
-  
-  
-  var addClient = '<script src="http://gabbertalk.com/845c0a1287da5c09377574389f310931/bootstrap.js?client=false"></script>'
-  $("body").append(addClient);
 
-  
 });
