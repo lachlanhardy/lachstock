f5cd8c621d9ba94d3634a65c79072fd5e246c8b8
Lachlan Hardy <lachlan@lachstock.com.au>
2013-08-11 22:48:20 +1000

    tightening up colour scheme and nav styles

--- lib/helpers/home_link.rb
+++ lib/helpers/home_link.rb
@@ -1,7 +1,7 @@
 module Lachstock
   module Helpers
     def home_link(text)
-      (@category == "home" ? '<span>' + text + '</span>' : '<a href="/" rel="home">' + text + '</a>')
+      (@category == "home" ? '<span class="pure-menu-selected">' + text + '</span>' : '<a href="/" rel="home">' + text + '</a>')
     end
   end
 end
